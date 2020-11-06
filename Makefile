7a51f8c0418774b8f0b5ac0e3f4a42fa9659f8d1
MartimRosado <martim.rosado@outlook.com>
2020-11-06 23:51:13 +0000

    make fpga + make clean added

--- Makefile
+++ Makefile
@@ -6,3 +6,14 @@ sim:
 
 sim-clean:
 	make -C $(SIM_DIR) clean
+
+sim-waves:
+	gtkwave $(SIM_DIR)/knn.vcd &
+
+fpga:
+	make -C $(FPGA_DIR) run DATA_W=$(DATA_W)
+
+fpga-clean:
+	make -C $(FPGA_DIR) clean
+
+clean: sim-clean fpga-clean
--- hardware/testbench/knn_tb.v
+++ hardware/testbench/knn_tb.v
@@ -5,14 +5,14 @@
 module knn_tb;
 
    localparam PER=10;
-   
+
    `CLOCK(clk, PER)
    `RESET(rst, 7, 10)
 
    `SIGNAL(KNN_ENABLE, 1)
    `SIGNAL(KNN_SAMPLE, 1)
    `SIGNAL_OUT(KNN_VALUE, 2*`DATA_W)
-   
+
    initial begin
 `ifdef VCD
       $dumpfile("knn.vcd");
@@ -27,22 +27,22 @@ module knn_tb;
       @(posedge clk) #1 KNN_SAMPLE = 1;
       @(posedge clk) #1 KNN_SAMPLE = 0;
 
-      //uncomment to fail the test 
+      //uncomment to fail the test
       //@(posedge clk) #1;
-      
+
       $write("Current time: %d; Timer value %d\n", $time, KNN_VALUE);
       #(1000*PER) @(posedge clk) #1 KNN_SAMPLE = 1;
       @(posedge clk) #1 KNN_SAMPLE = 0;
       $write("Current time: %d; Timer value %d\n", $time, KNN_VALUE);
 
-      if( KNN_VALUE == 1003) 
+      if( KNN_VALUE == 1003)
         $display("Test passed");
       else
         $display("Test failed: expecting knn value 1003 but got %d", KNN_VALUE);
-      
+
       $finish;
    end
-   
+
    //instantiate knn core
    knn_core knn0
      (
@@ -51,6 +51,6 @@ module knn_tb;
       .KNN_VALUE(KNN_VALUE),
       .clk(clk),
       .rst(rst)
-      );   
+      );
 
 endmodule
