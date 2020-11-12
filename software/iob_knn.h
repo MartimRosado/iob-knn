f5b862dd46682b89d366fe8ae5436ef94f9c8311
MartimRosado <martim.rosado@outlook.com>
2020-11-12 11:08:45 +0000

    same thing

--- software/iob_knn.h
+++ software/iob_knn.h
@@ -5,7 +5,6 @@ void knn_reset();
 void knn_start();
 void knn_stop();
 void knn_init(int base_address);
-
 void knn_set_value_A(unsigned int coordinate);
 void knn_set_value_B(unsigned int coordinate);
 int knn_read_distance();
