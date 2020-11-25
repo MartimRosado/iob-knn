31944613feb9ad0c36a7b22e129148942f6249e4
Miguel Reis <miguelareis98@gmail.com>
2020-11-25 18:54:39 +0000

    modified python prgram in order to process register banks

--- hardware/include/KNNsw_reg.v
+++ hardware/include/KNNsw_reg.v
@@ -4,4 +4,4 @@
 `SWREG_W(KNN_A,         DATA_W, 0) //Point A
 `SWREG_W(KNN_B,         DATA_W, 0) //Point B
 `SWREG_W(KNN_LABEL,     LABEL,  0) //Label point B
-`SWREG_R(KNN_DIST,      DATA_W, 0) //Distance between A and B
+`SWREG_BANK_R(KNN_INFO, LABEL, 0,10) 
