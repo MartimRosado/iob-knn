ba4a4a2d5ca70e62099703f91ee99130d95649c2
MartimRosado <martim.rosado@outlook.com>
2020-11-26 12:03:12 +0000

    missing changes

--- software/KNNsw_reg.h
+++ software/KNNsw_reg.h
@@ -6,4 +6,13 @@
 #define KNN_A 2
 #define KNN_B 3
 #define KNN_LABEL 4
-#define KNN_DIST 5
+#define KNN_INFO0 5
+#define KNN_INFO1 6
+#define KNN_INFO2 7
+#define KNN_INFO3 8
+#define KNN_INFO4 9
+#define KNN_INFO5 10
+#define KNN_INFO6 11
+#define KNN_INFO7 12
+#define KNN_INFO8 13
+#define KNN_INFO9 14
--- software/iob_knn.h
+++ software/iob_knn.h
@@ -6,4 +6,5 @@ void knn_start();
 void knn_stop();
 void knn_init(int base_address);
 void knn_set_TestP(unsigned int coordinate);
-int knn_dist_DataP(unsigned int coordinate);
+void knn_set_DataP(unsigned int coordinate, char label);
+char knn_read_Label(unsigned int offset);
