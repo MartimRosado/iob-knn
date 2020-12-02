28e279998ecb7de8fbfb9bd8f18210e09d2bac9c
Miguel Reis <miguelareis98@gmail.com>
2020-12-02 13:38:17 +0000

    Added BANK_W and CUBE_R

--- hardware/include/KNNsw_reg.v
+++ hardware/include/KNNsw_reg.v
@@ -1,7 +1,8 @@
 //START_TABLE sw_reg
 `SWREG_W(KNN_RESET,          1, 0) //KNN soft reset
 `SWREG_W(KNN_ENABLE,         1, 0) //KNN enable
-`SWREG_W(KNN_A,         DATA_W, 0) //Point A
+// `SWREG_W(KNN_A,         DATA_W, 0) //Point A
 `SWREG_W(KNN_B,         DATA_W, 0) //Point B
 `SWREG_W(KNN_LABEL,     LABEL,  0) //Label point B
-`SWREG_BANK_R(KNN_INFO, LABEL, 0, N_Neighbours)
+`SWREG_BANKW(KNN_A, DATA_W, 0,10) // Point A Bank
+`SWREG_CUBER(KNN_INFO, LABEL, 0, 10,10)
